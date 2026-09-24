94a8f3bd46934fcfa5c3a2d40997f01a1f194c1f
MarcusOrion <manvati.marco@gmail.com>
2026-09-24 15:17:00 +0200

    add thrid exercise (aggiungere tre bottoni che cambiano il testo a sinistra\centro\destra se cliccati)

--- react-binding/src/components/Counter.jsx
+++ react-binding/src/components/Counter.jsx
@@ -22,7 +22,7 @@ export default function Counter() {
 }*/
 
   // Crea un bottone che alterni la propria classe stilistica (es. da primary a success) ad ogni click, mutandone dinamicamente l'aspetto grafico
-  const [isPrimary, setIsPrimary] = useState(true);
+  /*const [isPrimary, setIsPrimary] = useState(true);
   return (
     <div className="container mt-5 text-center">
       <button
@@ -31,6 +31,39 @@ export default function Counter() {
       >
         {isPrimary ? "Cliccami" : "Fatto!"}
       </button>
+    </div>
+  );*/
+
+  // Creare un componente con tre pulsanti ed un paragrafo. Creare una variabile di stato reattiva per gestire l’allineamento del paragrafo. Per ogni pulsante, impostare l’evento onClick e impostare l’allineamento in base al pulsante cliccato
+  const [alignment, setAlignment] = useState("text-start");
+  return (
+    <div className="container mt-5 text-center">
+      <div className="row justify-content-center">
+        <div className="col-md-6 text-center">
+          <h2 className="mb-4">Allineamento Testo</h2>
+          <button
+            className="btn btn-primary me-3"
+            onClick={() => setAlignment("text-start")}
+          >
+            Metti a sinistra
+          </button>
+          <button
+            className="btn btn-primary me-3"
+            onClick={() => setAlignment("text-center")}
+          >
+            Centra
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={() => setAlignment("text-end")}
+          >
+            Metti a destra
+          </button>
+        </div>
+      </div>
+      <div className="card p-3 shadow-sm mt-2">
+        <p className={`m-0 ${alignment}`}>Lorem Ipsum eccetera eccetera.</p>
+      </div>
     </div>
   );
 }
